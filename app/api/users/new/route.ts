b6053193a2e784cd47ec98ac4d45572786fff086
GideonAdonteng <adontenggideon57@gmail.com>
2024-01-04 10:25:25 -0800

    Added comments

--- app/api/users/new/route.ts
+++ app/api/users/new/route.ts
@@ -3,25 +3,34 @@ import { connectDB } from "@/lib/database";
 import bcrypt from "bcrypt";
 
 export const POST = async (request: Request) => {
+  // Destructure user details from request
   const { username, firstname, lastname, email, password, avatarUrl, roles } =
     await request.json();
 
+  // Return an error is any these details is missing
   if (!username || !firstname || !lastname || !email || !password) {
     return new Response("Please fill all required fields", { status: 400 });
   }
 
+  // Connecting to the database
   await connectDB();
+
+  // Check if username already exists
   const userExists = await UserModel.findOne({ username })
     .collation({ locale: "en", strength: 2 })
     .lean()
     .exec();
 
+  // If username exists return an error
   if (userExists) {
     return new Response("Username already taken", { status: 409 });
   }
 
+  // If username doesn't exist we go on to hash the password
+  // We don't want to store user passwords in our database
   const hashedPassword = await bcrypt.hash(password, 10);
 
+  //Create user object with data provided
   const userObj =
     !Array.isArray(roles) || !roles.length || !avatarUrl
       ? { username, firstname, lastname, email, password: hashedPassword }
@@ -35,6 +44,7 @@ export const POST = async (request: Request) => {
           roles,
         };
 
+  // Create and save user to the database
   const user = await UserModel.create(userObj);
 
   if (user) {
--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -5,8 +5,10 @@ export const GET = async () => {
   try {
     await connectDB();
 
+    // Get user details excluding password from database
     const users = await UserModel.find().select("-password").lean();
 
+    // If there are no users return an error
     if (!users?.length) return new Response("No users found", { status: 400 });
 
     return new Response(JSON.stringify(users), { status: 200 });
