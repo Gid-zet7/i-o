0a1fbc55d4fd9b12184ee4e0e955fe87210e2895
GideonAdonteng <adontenggideon57@gmail.com>
2024-01-04 11:51:50 -0800

    Added employee route to fetch employess on request

---
+++ app/api/employees/route.ts
@@ -0,0 +1,17 @@
+import Employee from "@/models/employeeModel";
+import { connectDB } from "@/lib/database";
+
+export const GET = async () => {
+  try {
+    await connectDB();
+
+    const employees = await Employee.find().populate("user").lean();
+
+    if (!employees?.length)
+      return new Response("No employees found", { status: 400 });
+
+    return new Response(JSON.stringify(employees), { status: 200 });
+  } catch (error) {
+    return new Response("Failed to fetch employees", { status: 500 });
+  }
+};
