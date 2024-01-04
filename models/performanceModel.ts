f4ed83a4aa95ac237b1f24243864ec6a40e386a1
GideonAdonteng <adontenggideon57@gmail.com>
2024-01-04 09:23:53 -0800

    Added performance schema

---
+++ models/performanceModel.ts
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+const Schema = mongoose.Schema;
+
+interface PerformanceInterface {
+  employee: mongoose.Types.ObjectId | null | undefined;
+  date: Date;
+  feedback: string;
+  ratings: number;
+}
+
+const performanceSchema = new Schema<PerformanceInterface>(
+  {
+    employee: { type: Schema.Types.ObjectId, ref: "employee" },
+    date: { type: Date, require: true },
+    feedback: { type: String, require: true },
+    ratings: { type: Number, require: true },
+  },
+  { timestamps: true }
+);
+
+const Performance =
+  mongoose.models.performance ||
+  mongoose.model<PerformanceInterface>("performance", performanceSchema);
+
+export default Performance;
