17fd39e412d74ebd17cc0b1893aaf0faccba048f
GideonAdonteng <adontenggideon57@gmail.com>
2024-01-04 21:50:54 -0800

    modified department-create routes

--- app/api/departments/create/route.ts
+++ app/api/departments/create/route.ts
@@ -1,6 +1,3 @@
-import UserModel from "@/models/userModel";
-import Employee from "@/models/employeeModel";
-import Manager from "@/models/managerModel";
 import { connectDB } from "@/lib/database";
 import Department from "@/models/departmentModel";
 
@@ -16,30 +13,6 @@ export const POST = async (request: Request) => {
   // Connecting to the database
   await connectDB();
 
-  // Check if the 'head of deparment to be' is a user
-  const findUser = await UserModel.findOne({
-    username: head_of_department,
-  }).exec();
-
-  //   If not return an error
-  if (!findUser) return new Response("Sign up first", { status: 409 });
-
-  //   Check if the 'head of deparment to be' is an employee
-  const findEmployee = await Employee.findOne({ user: findUser })
-    .populate("user")
-    .exec();
-
-  if (!findEmployee)
-    return new Response("Register as an employee first", { status: 409 });
-
-  //   Check if the 'head of deparment to be' is a manager
-  const findManager = await Manager.findOne({ employee: findEmployee }).exec();
-
-  if (!findManager)
-    return new Response("Only managers can be head of deparment ", {
-      status: 409,
-    });
-
   // Watchout for duplicate departments
   const duplicate = await Department.findOne({ name })
     .collation({ locale: "en", strength: 2 })
@@ -54,7 +27,7 @@ export const POST = async (request: Request) => {
   // Create department object
   const departmentObj = {
     name,
-    head_of_department: findManager,
+    head_of_department,
     description,
   };
 
