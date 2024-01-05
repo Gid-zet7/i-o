8474433fc748ba1458039849210d9fa90041dfb2
GideonAdonteng <adontenggideon57@gmail.com>
2024-01-04 21:53:52 -0800

    Updated employee api to fetch user by id

---
+++ app/api/employees/[id]/route.ts
@@ -0,0 +1,21 @@
+import Employee from "@/models/employeeModel";
+import { connectDB } from "@/lib/database";
+
+type Props = {
+  params: {
+    id: string;
+  };
+};
+
+export const GET = async (request: Request, { params: { id } }: Props) => {
+  await connectDB();
+
+  const employee = await Employee.findById(id)
+    .populate("user")
+    .populate("department")
+    .lean();
+
+  if (!employee) return new Response("Employee not found", { status: 400 });
+
+  return new Response(JSON.stringify(employee), { status: 200 });
+};
