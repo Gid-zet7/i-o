ab2e803c49e89a78c59e6cd94aaca9fc9351c1c3
GideonAdonteng <adontenggideon57@gmail.com>
2024-01-05 20:32:31 -0800

    Added subfield component

---
+++ components/Subfields.tsx
@@ -0,0 +1,24 @@
+"use client";
+import { SubLinks } from "@/constant";
+
+const Subfields = () => {
+  return (
+    <div className="flexBetween w-full gap-5 flex-wrap">
+      <ul className="flex gap-2 overflow-auto ">
+        {SubLinks.map((link, i) => (
+          <button
+            key={i}
+            type="button"
+            className="px-4 py-1 rounded-full capitalize whitespace-nowrap font-normal mt-3"
+          >
+            <span className="bg-gradient-to-r from-indigo-500 to-indigo-300 bg-clip-text text-transparent ">
+              {link.text}
+            </span>
+          </button>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Subfields;
