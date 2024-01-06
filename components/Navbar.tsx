6acc878e081b5f44bd9f2fd65124ed2bdced781f
GideonAdonteng <adontenggideon57@gmail.com>
2024-01-05 20:18:14 -0800

    Added styles to navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 const Navbar = async () => {
   return (
-    <nav className="navbar sticky top-0 z-10 backdrop-blur-sm">
+    <nav className="py-5 px-8 border-b gap-4 sticky top-0 z-10 backdrop-blur-sm">
       <div className="mx-auto flex max-w-4xl items-center justify-between">
         <h1 className="text-3xl">
           <Link href="/test">I/O</Link>
