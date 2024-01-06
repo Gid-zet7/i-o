015ab57c80f3aa13a8fc6caa499c0e9426fcc058
GideonAdonteng <adontenggideon57@gmail.com>
2024-01-05 20:34:16 -0800

    Added sublinks

---
+++ constant/index.ts
@@ -0,0 +1,11 @@
+export const SubLinks = [
+  { href: "/", key: "Job Analysis", text: "Job Analysis" },
+  { href: "/", key: "Selection", text: "Selection and Placement" },
+  { href: "/", key: "Health", text: "Occupational Health and Safety" },
+  { href: "/", key: "Consumer", text: "Consumer Behaviour" },
+  { href: "/", key: "Taining", text: "Training and Development" },
+  { href: "/", key: "Relations", text: "Human Relations" },
+  { href: "/", key: "Motivation", text: "Motivation and Commitment" },
+  { href: "/", key: "Appraisal", text: "Performance Appraisal" },
+  { href: "/", key: "Satisfaction", text: "Job Satisfaction" },
+];
