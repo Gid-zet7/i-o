6fefbef8a52ca3e084cbe3e92db004a0d529bca9
GideonAdonteng <adontenggideon57@gmail.com>
2024-01-06 13:51:12 -0800

    Added actions to fetch data from server

---
+++ lib/actions.ts
@@ -0,0 +1,75 @@
+export const getAllUsers = async () => {
+  const users = await fetch("http://localhost:3000/api/users", {
+    next: { revalidate: 10 },
+  });
+
+  if (!users.ok) throw new Error("Failed to fetch users");
+
+  return users.json();
+};
+
+export const getUser = async (userId: string) => {
+  const user = await fetch(`http://localhost:3000/api/users/${userId}`, {
+    next: { revalidate: 10 },
+  });
+
+  if (!user.ok) return undefined;
+
+  return user.json();
+};
+
+export const getAllEmployees = async () => {
+  const employees = await fetch("http://localhost:3000/api/employees", {
+    next: { revalidate: 10 },
+  });
+
+  if (!employees.ok) throw new Error("Failed to fetch employees");
+
+  return employees.json();
+};
+
+export const getEmployee = async (employeeId: string) => {
+  const employee = await fetch(
+    `http://localhost:3000/api/employees/${employeeId}`,
+    {
+      next: { revalidate: 10 },
+    }
+  );
+
+  if (!employee.ok) return undefined;
+
+  return employee.json();
+};
+
+export const getAllManagers = async () => {
+  const managers = await fetch("http://localhost:3000/api/managers", {
+    next: { revalidate: 10 },
+  });
+
+  if (!managers.ok) throw new Error("Failed to fetch managers");
+
+  return managers.json();
+};
+
+export const getManger = async (managerId: string) => {
+  const manager = await fetch(
+    `http://localhost:3000/api/managers/${managerId}`,
+    {
+      next: { revalidate: 10 },
+    }
+  );
+
+  if (!manager.ok) return undefined;
+
+  return manager.json();
+};
+
+export const getAllForms = async () => {
+  const forms = await fetch("http://localhost:3000/api/appraisal-form", {
+    next: { revalidate: 30 },
+  });
+
+  if (!forms.ok) throw new Error("Failed to fetch forms");
+
+  return forms.json();
+};
