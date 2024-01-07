7203c0252137a424491be4e6a9cdf87a73361a20
GideonAdonteng <adontenggideon57@gmail.com>
2024-01-07 01:09:13 -0800

    Modified new employee form component; wrapped everythin in a div container

--- components/NewEmployeeForm.tsx
+++ components/NewEmployeeForm.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 import Link from "next/link";
 import { useState } from "react";
 import { useRouter } from "next/navigation";
-import { getAllUsers } from "@/lib/actions";
+import { getAllDepartments, getAllUsers } from "@/lib/actions";
 
 export default async function NewEmployeeForm() {
   const [username, setUserName] = useState("");
@@ -52,8 +52,10 @@ export default async function NewEmployeeForm() {
   };
 
   const usersData: Promise<User[]> = getAllUsers();
-
   const users = await usersData;
+
+  const departmentsData: Promise<Department[]> = getAllDepartments();
+  const departments = await departmentsData;
 
   const options = users.map((user) => {
     return (
@@ -63,93 +65,105 @@ export default async function NewEmployeeForm() {
     );
   });
 
+  const deptOptions = departments.map((department) => {
+    return (
+      <option key={department._id} value={department.name}>
+        {department.name}
+      </option>
+    );
+  });
+
   return (
-    <div className="p-4 mt-10 flex flex-col-reverse md:flex-row w-full">
-      <div className="flex-1">
-        <h1 className="text-xl font-bold my-4 grid place-content-center">
-          Add Employee
-        </h1>
+    <div className="max-w-5xl mx-auto p-3">
+      <div className="p-4 mt-10 flex flex-col-reverse md:flex-row w-full ">
+        <div className="flex-1">
+          <h1 className="text-xl font-bold my-4 grid place-content-center">
+            Add Employee
+          </h1>
 
-        <form onSubmit={handleSubmit} className="flex flex-col gap-4 ">
-          <label htmlFor="user" className="">
-            Select a user from the users list below. <br />
-            <span className="text-xs text-slate-400">
-              Employees need to sign up as users first
-            </span>
-          </label>
-          <select
-            className="p-1 rounded text-black outline-none w-5/6 sm:w-3/5"
-            id="user"
-            name="user"
-            onChange={(e) => setUserName(e.target.value)}
-            value={username}
-          >
-            <option>--Select User--</option>
-            {options}
-          </select>
-          <label htmlFor="department" className="">
-            Choose department
-          </label>
-          <input
-            className="p-1 rounded text-black outline-none w-5/6 sm:w-3/5"
-            onChange={(e) => setDepartment(e.target.value)}
-            type="text"
-            id="department"
-            name="department"
-            placeholder="department..."
-          />
-          <label htmlFor="position" className="">
-            Position
-          </label>
-          <input
-            className="p-1 rounded text-black outline-none w-5/6 sm:w-3/5"
-            onChange={(e) => setPosition(e.target.value)}
-            id="position"
-            name="position"
-            type="text"
-            placeholder="eg.project manager..."
-          />
-          <label htmlFor="skills" className="">
-            Skills
-          </label>
-          <input
-            className="p-1 rounded text-black outline-none w-5/6 sm:w-3/5"
-            onChange={(e) => setSkills(e.target.value)}
-            id="skills"
-            name="skills"
-            type="text"
-            placeholder="skills..."
-          />
-          <label htmlFor="startDate">Start Date:</label>
-          <input
-            className="p-1 rounded text-black outline-none w-5/6 sm:w-3/5"
-            onChange={(e) => setStartDate(e.target.value)}
-            id="startDate"
-            name="startDate"
-            type="date"
-          />
-          <button className="bg-amber-300 text-black font-bold cursor-pointer px-6 py-2 w-5/6 sm:w-3/5 hover:bg-opacity-90">
-            Save
-          </button>
+          <form onSubmit={handleSubmit} className="flex flex-col gap-4 ">
+            <label htmlFor="user" className="">
+              Select a user from the users list below. <br />
+              <span className="text-xs text-slate-400">
+                Employees need to sign up as users first
+              </span>
+            </label>
+            <select
+              className="p-1 rounded text-black outline-none w-5/6 sm:w-3/5"
+              id="user"
+              name="user"
+              onChange={(e) => setUserName(e.target.value)}
+              value={username}
+            >
+              <option>--Select User--</option>
+              {options}
+            </select>
+            <label htmlFor="department" className="">
+              Choose department
+            </label>
+            <select
+              className="p-1 rounded text-black outline-none w-5/6 sm:w-3/5"
+              id="department"
+              name="department"
+              onChange={(e) => setDepartment(e.target.value)}
+              value={department}
+            >
+              <option>--Select Department--</option>
+              {deptOptions}
+            </select>
+            <label htmlFor="position" className="">
+              Position
+            </label>
+            <input
+              className="p-1 rounded text-black outline-none w-5/6 sm:w-3/5"
+              onChange={(e) => setPosition(e.target.value)}
+              id="position"
+              name="position"
+              type="text"
+              placeholder="eg.project manager..."
+            />
+            <label htmlFor="skills" className="">
+              Skills
+            </label>
+            <input
+              className="p-1 rounded text-black outline-none w-5/6 sm:w-3/5"
+              onChange={(e) => setSkills(e.target.value)}
+              id="skills"
+              name="skills"
+              type="text"
+              placeholder="skills..."
+            />
+            <label htmlFor="startDate">Start Date:</label>
+            <input
+              className="p-1 rounded text-black outline-none w-5/6 sm:w-3/5"
+              onChange={(e) => setStartDate(e.target.value)}
+              id="startDate"
+              name="startDate"
+              type="date"
+            />
+            <button className="bg-amber-300 text-black font-bold cursor-pointer px-6 py-2 w-5/6 sm:w-3/5 hover:bg-opacity-90">
+              Save
+            </button>
 
-          {error && (
-            <div className="bg-red-500 text-white w-fit text-sm py-1 px-3 rounded-md mt-2">
-              {error}
-            </div>
-          )}
+            {error && (
+              <div className="bg-red-500 text-white w-fit text-sm py-1 px-3 rounded-md mt-2">
+                {error}
+              </div>
+            )}
 
-          <Link className="text-sm mt-3 text-left" href={"/"}>
-            Back to Home
-          </Link>
-        </form>
+            <Link className="text-sm mt-3 text-left" href={"/"}>
+              Back to Home
+            </Link>
+          </form>
+        </div>
+        <Image
+          src="/undraw_add_user_re_5oib.svg"
+          width={330}
+          height={80}
+          className=""
+          alt="add employee image"
+        />
       </div>
-      <Image
-        src="/undraw_add_user_re_5oib.svg"
-        width={330}
-        height={80}
-        className=""
-        alt="add employee image"
-      />
     </div>
   );
 }
