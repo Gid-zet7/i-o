34cb9a75d33b601bf3a31e461baedf833151461e
GideonAdonteng <adontenggideon57@gmail.com>
2024-01-07 15:15:47 -0800

    Clean code

--- app/employees/page.tsx
+++ app/employees/page.tsx
@@ -20,13 +20,6 @@ export default async function EmployeesPage() {
         <div className="flex flex-wrap gap-8 mt-10">
           {employees.map((employee) => {
             return (
-              // <div key={employee._id}>
-              //   <p>{employee.user.username} </p>
-              //   <p>{employee.position} </p>
-              //   <p>{employee.department} </p>
-              //   {/* <p>{employee.skills} </p> */}
-              // </div>
-              // <Link href={`http://localhost:3000/employees/${employee._id}`}>
               <Card
                 id={employee._id}
                 // username={employee.user.username}
@@ -35,7 +28,6 @@ export default async function EmployeesPage() {
                 position={employee.position}
                 image={employee.user.avatarUrl}
               />
-              // </Link>
             );
           })}
         </div>
