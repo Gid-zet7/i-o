0f29505d7dc277a82d61f89fd3348680af730213
GideonAdonteng <adontenggideon57@gmail.com>
2024-01-08 09:47:46 -0800

    Updated card to display default image if employee does not have an image

--- components/Card.tsx
+++ components/Card.tsx
@@ -26,8 +26,8 @@ const Card = ({
 }: Props) => {
   return (
     <div className="flexCenter flex-col rounded-2xl shadow-md dark:bg-slate-100 dark:text-black min-w-1/4">
-      <Link href={`/employees/${id}`} className="flexCenter group relative">
-        {image ? (
+      {image ? (
+        <Link href={`/employees/${id}`} className="flexCenter group relative">
           <Image
             src={image}
             width={130}
@@ -35,16 +35,20 @@ const Card = ({
             className="object-cover rounded-t-2xl w-auto"
             alt="employee image"
           />
-        ) : (
-          <Image
-            src="/undraw_male_avatar_g98d.svg"
-            width={130}
-            height={80}
-            className="object-cover rounded-t-2xl w-auto"
-            alt="employee image"
-          />
-        )}
-      </Link>
+        </Link>
+      ) : (
+        <div className="flexCenter flex-col rounded-full shadow-md dark:bg-slate-100 dark:text-black min-w-1/4">
+          <Link href={`/managers/${id}`} className="flexCenter group relative">
+            <Image
+              src="/undraw_male_avatar_g98d.svg"
+              width={180}
+              height={80}
+              className="object-cover rounded-t-2xl w-aut"
+              alt="employee image"
+            />
+          </Link>
+        </div>
+      )}
 
       <div className="flexBetween px-2 mt-3 font-semibold text-sm">
         <div>
