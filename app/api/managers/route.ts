58ca05d38dfe53168d196d2519d15c6df6897d05
GideonAdonteng <adontenggideon57@gmail.com>
2024-01-08 14:52:18 -0800

    Updated managers api to populate team and user details

--- app/api/managers/route.ts
+++ app/api/managers/route.ts
@@ -1,39 +1,32 @@
 import Manager from "@/models/managerModel";
 import { connectDB } from "@/lib/database";
-import Employee from "@/models/employeeModel";
-import UserModel from "@/models/userModel";
 
 export const GET = async () => {
   try {
     await connectDB();
 
     const managers = await Manager.find()
-      .populate("employee")
-      .populate("team")
-      .populate("team.user")
-      .lean();
+      .populate({
+        path: "employee",
+        populate: {
+          path: "user",
+          model: "user",
+        },
+      })
+      .populate({
+        path: "team",
+        populate: {
+          path: "user",
+          model: "user",
+        },
+      })
+      .lean()
+      .exec();
 
     if (!managers?.length)
       return new Response("No Managers found", { status: 400 });
 
-    const managersWithUser = await Promise.all(
-      managers.map(async (manager) => {
-        const employee: any = await Employee.findById(manager.employee)
-          .lean()
-          .exec();
-        const user: any = await UserModel.findById(employee?.user)
-          .lean()
-          .exec();
-
-        return {
-          ...manager,
-          username: user?.username,
-          firstname: user?.firstname,
-          lastname: user?.lastname,
-        };
-      })
-    );
-    return new Response(JSON.stringify(managersWithUser), { status: 200 });
+    return new Response(JSON.stringify(managers), { status: 200 });
   } catch (error) {
     return new Response("Failed to fetch managers", { status: 500 });
   }
