87e66ca1aad56884b838fddcab531865cc6bd2c8
GideonAdonteng <adontenggideon57@gmail.com>
2024-01-08 22:54:33 -0800

    Added first and last name fields, I feel this is where it should belong

--- models/employeeModel.ts
+++ models/employeeModel.ts
@@ -5,6 +5,8 @@ const Schema = mongoose.Schema;
 const employeeSchema = new Schema<Employee>(
   {
     user: { type: Schema.Types.ObjectId, ref: "user" },
+    firstname: { type: String, required: true, minlength: 3 },
+    lastname: { type: String, required: true, minlength: 3 },
     department: { type: Schema.Types.ObjectId, ref: "department" },
     position: { type: String, require: true },
     skills: [String],
