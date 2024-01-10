92d7c0cfd4d555bcd11735dbd681a0d8037de5c9
GideonAdonteng <adontenggideon57@gmail.com>
2024-01-10 04:37:08 -0800

    Updated navbar to display sign in button when users are not signed in and display a profile menu when users sign in

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,13 +1,25 @@
-import Image from "next/image";
 import Link from "next/link";
+import ProfileMenu from "./ProfileMenu";
+import { getCurrentUser } from "@/lib/session";
+import AuthProviders from "./AuthProviders";
 
 const Navbar = async () => {
+  const session = await getCurrentUser();
   return (
     <nav className="py-5 px-8 border-b gap-4 sticky top-0 z-10 backdrop-blur-sm">
       <div className="mx-auto flex max-w-4xl items-center justify-between">
         <h1 className="text-3xl">
           <Link href="/">I/O</Link>
         </h1>
+        <div className="flexCenter gap-4">
+          {session?.user ? (
+            <>
+              <ProfileMenu session={session} />
+            </>
+          ) : (
+            <AuthProviders />
+          )}
+        </div>
       </div>
     </nav>
   );
