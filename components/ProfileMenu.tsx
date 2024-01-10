a12d83deb73a6417c5ea59ff21991476db6ce084
GideonAdonteng <adontenggideon57@gmail.com>
2024-01-10 14:09:10 -0800

    Updated profile menus details and added styles

--- components/ProfileMenu.tsx
+++ components/ProfileMenu.tsx
@@ -3,12 +3,13 @@
 import Image from "next/image";
 import { signOut } from "next-auth/react";
 import { useState } from "react";
+import Link from "next/link";
 
 const ProfileMenu = ({ session }: { session: SessionInterface }) => {
   const [openModal, setOpenModal] = useState(false);
 
   return (
-    <div className="flexCenter z-10 flex-col relative">
+    <div className="flexCenter flex-col absolute top-4">
       <div className="rounded-full">
         <button
           className="flexCenter "
@@ -23,12 +24,12 @@ const ProfileMenu = ({ session }: { session: SessionInterface }) => {
               alt="user profile image"
             />
           )}
-          <p className="font-semibold">{session?.user?._doc.username}</p>
+          {/* <p className="font-semibold">{session?.user?._doc.username}</p> */}
         </button>
 
         {openModal && (
           <>
-            <div className="flex flex-col items-center gap-y-4">
+            <div className="flex flex-col items-center gap-y-4 bg-slate-100 p-12 dark:bg-black rounded-3xl">
               {session?.user?._doc.avatarUrl && (
                 <Image
                   src={session?.user?._doc.avatarUrl}
@@ -40,17 +41,23 @@ const ProfileMenu = ({ session }: { session: SessionInterface }) => {
               )}
               <p className="font-semibold">{session?.user?._doc.username}</p>
               <p className="font-semibold">{session?.user?._doc.email}</p>
-            </div>
-            <div className="w-full flexStart border-t border-nav-border mt-5 pt-5">
-              <button
-                type="button"
-                className="text-sm"
-                onClick={() =>
-                  signOut({ callbackUrl: "http://localhost:3000" })
-                }
-              >
-                Sign out
-              </button>
+
+              <p>{session.user._doc.active}</p>
+
+              <Link href={`/employees/${session.user._doc._id}`}>
+                <p>Edit Profile</p>
+              </Link>
+              <div className="w-full flexStart border-t border-nav-border mt-5 pt-5">
+                <button
+                  type="button"
+                  className="text-sm bg-red-500 px-4 py-2 rounded-sm hover:opacity-90"
+                  onClick={() =>
+                    signOut({ callbackUrl: "http://localhost:3000" })
+                  }
+                >
+                  Sign out
+                </button>
+              </div>
             </div>
           </>
         )}
