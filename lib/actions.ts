2e4388433f55927442f9fe1aa44f566e49bd6d88
GideonAdonteng <adontenggideon57@gmail.com>
2024-01-10 13:58:42 -0800

    Updated actions

--- lib/actions.ts
+++ lib/actions.ts
@@ -1,118 +1,113 @@
+"use server";
 import { getServerSession } from "next-auth";
 import { authOptions } from "./session";
 
 // ---------------Users----------------------
 export const getAllUsers = async () => {
   const session: SessionInterface | null = await getServerSession(authOptions);
-  const users = await fetch("http://localhost:3000/api/users", {
+  const res = await fetch("http://localhost:3000/api/users", {
     method: "GET",
     headers: {
       Authorization: `Bearer ${session?.user?.accessToken}`,
     },
   });
 
-  if (!users.ok) throw new Error("Failed to fetch users");
+  if (!res.ok) throw new Error("Failed to fetch users");
 
-  return users.json();
+  return res.json();
 };
 
 export const getUser = async (userId: string) => {
   const session: SessionInterface | null = await getServerSession(authOptions);
-  const user = await fetch(`http://localhost:3000/api/users/${userId}`, {
+  const res = await fetch(`http://localhost:3000/api/users/${userId}`, {
     method: "GET",
     headers: {
       Authorization: `Bearer ${session?.user?.accessToken}`,
     },
   });
 
-  if (!user.ok) return undefined;
+  if (!res.ok) return undefined;
 
-  return user.json();
+  return res.json();
 };
 
 // ---------------Employees----------------------
 export const getAllEmployees = async () => {
   const session: SessionInterface | null = await getServerSession(authOptions);
-  const employees = await fetch("http://localhost:3000/api/employees", {
+  let res = await fetch("http://localhost:3000/api/employees", {
     method: "GET",
     headers: {
       Authorization: `Bearer ${session?.user?.accessToken}`,
     },
   });
 
-  if (!employees.ok) throw new Error("Failed to fetch employees");
+  if (!res.ok) throw new Error("Failed to fetch employees");
 
-  return employees.json();
+  return res.json();
 };
 
 export const getEmployee = async (employeeId: string) => {
   const session: SessionInterface | null = await getServerSession(authOptions);
-  const employee = await fetch(
-    `http://localhost:3000/api/employees/${employeeId}`,
-    {
-      method: "GET",
-      headers: {
-        Authorization: `Bearer ${session?.user?.accessToken}`,
-      },
-    }
-  );
+  let res = await fetch(`http://localhost:3000/api/employees/${employeeId}`, {
+    method: "GET",
+    headers: {
+      Authorization: `Bearer ${session?.user?.accessToken}`,
+    },
+  });
 
-  if (!employee.ok) return undefined;
+  if (!res.ok) return undefined;
 
-  return employee.json();
+  return res.json();
 };
 
 // ---------------Managers----------------------
 export const getAllManagers = async () => {
   const session: SessionInterface | null = await getServerSession(authOptions);
-  const managers = await fetch("http://localhost:3000/api/managers", {
+  let res = await fetch("http://localhost:3000/api/managers", {
     method: "GET",
     headers: {
       Authorization: `Bearer ${session?.user?.accessToken}`,
     },
   });
 
-  if (!managers.ok) throw new Error("Failed to fetch managers");
+  if (!res.ok) throw new Error("Failed to fetch managers");
 
-  return managers.json();
+  return res.json();
 };
 
 export const getManager = async (managerId: string) => {
   const session: SessionInterface | null = await getServerSession(authOptions);
-  const manager = await fetch(
-    `http://localhost:3000/api/managers/${managerId}`,
-    {
-      method: "GET",
-      headers: {
-        Authorization: `Bearer ${session?.user?.accessToken}`,
-      },
-    }
-  );
+  let res = await fetch(`http://localhost:3000/api/managers/${managerId}`, {
+    method: "GET",
+    headers: {
+      Authorization: `Bearer ${session?.user?.accessToken}`,
+    },
+  });
 
-  if (!manager.ok) return undefined;
+  if (!res.ok) return undefined;
 
-  return manager.json();
+  return res.json();
 };
 
 // --------------------------Department---------------------------
 
 export const getAllDepartments = async () => {
   const session: SessionInterface | null = await getServerSession(authOptions);
-  const departments = await fetch("http://localhost:3000/api/departments", {
+  let res = await fetch("http://localhost:3000/api/departments", {
     method: "GET",
     headers: {
       Authorization: `Bearer ${session?.user?.accessToken}`,
     },
   });
 
-  if (!departments.ok) throw new Error("Failed to fetch departments");
+  if (!res.ok) throw new Error("Failed to fetch departments");
 
-  return departments.json();
+  return res.json();
 };
 
 export const getDepartment = async (departmentId: string) => {
   const session: SessionInterface | null = await getServerSession(authOptions);
-  const department = await fetch(
+  let res = await fetch(
     `http://localhost:3000/api/departments/${departmentId}`,
     {
       method: "GET",
@@ -122,22 +117,22 @@ export const getDepartment = async (departmentId: string) => {
     }
   );
 
-  if (!department.ok) return undefined;
+  if (!res.ok) return undefined;
 
-  return department.json();
+  return res.json();
 };
 
 // -----------------------------Forms----------------------
 export const getAllForms = async () => {
   const session: SessionInterface | null = await getServerSession(authOptions);
-  const forms = await fetch("http://localhost:3000/api/appraisal-form", {
+  let res = await fetch("http://localhost:3000/api/appraisal-form", {
     method: "GET",
     headers: {
       Authorization: `Bearer ${session?.user?.accessToken}`,
     },
   });
 
-  if (!forms.ok) throw new Error("Failed to fetch forms");
+  if (!res.ok) throw new Error("Failed to fetch forms");
 
-  return forms.json();
+  return res.json();
 };
