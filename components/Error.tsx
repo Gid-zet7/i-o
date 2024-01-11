4f65b0777db538480b4c38714930f75e0a9cd11d
GideonAdonteng <adontenggideon57@gmail.com>
2024-01-11 15:06:19 -0800

    Added the error page to handle errors gracefully

--- components/Error.tsx
+++ components/Error.tsx
@@ -6,16 +6,24 @@ import { signIn } from "next-auth/react";
 export default function ErrorPage({ error }: { error: Error }) {
   return (
     <div className="grid place-content-center mt-32 gap-8">
-      <div className="flex flex-col items-center gap-8 shadow-indigo-500/50 border-2 dark:border-1 p-8 rounded-3xl">
-        <h1 className="text-3xl font-semibold">OOPS {error.message}</h1>
-        <p className="text-xl text-red-500">Your Session expired!</p>
+      <div className="flex flex-col items-center gap-8 shadow-xl  p-8 rounded-3xl bg-slate-200">
+        <h1 className="text-3xl font-semibold text-black">
+          OOPS {error.message}
+        </h1>
+        <p className="text-xl text-black font-semibold">
+          Your Session expired!
+        </p>
         <Image
-          src={"/undraw_access_denied_re_awnf.svg"}
+          src={"/warning-alert-svgrepo-com.svg"}
           alt="An illustration of man who has been denied access"
           width={100}
           height={60}
         />
-        <Button title="Please in sign in again" handleClick={() => signIn()} />
+        <Button
+          title="Please in sign in again"
+          bgColor="bg-red-600"
+          handleClick={() => signIn()}
+        />
       </div>
     </div>
   );
