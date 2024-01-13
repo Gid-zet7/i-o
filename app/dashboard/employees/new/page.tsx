a99bfa7e5cd2ab30345e81e682e7b46198cc0e7a
GideonAdonteng <adontenggideon57@gmail.com>
2024-01-13 08:20:16 -0800

    Update page to fetch dapartments data

--- app/dashboard/employees/new/page.tsx
+++ app/dashboard/employees/new/page.tsx
@@ -1,5 +1,11 @@
 import NewEmployeeForm from "@/components/NewEmployeeForm";
+import { getAllDepartments } from "@/lib/actions";
+import { notFound } from "next/navigation";
 
-export default function AddEmployee() {
-  return <NewEmployeeForm />;
+export default async function AddEmployee() {
+  const departmentsData: Promise<Department> = getAllDepartments();
+  const departments: Department = await departmentsData;
+
+  if (!departments) return notFound();
+  return <NewEmployeeForm departments={departments} />;
 }
