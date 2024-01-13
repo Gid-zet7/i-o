168eaad83fd346b62fd680acacf5e6956a0900c6
GideonAdonteng <adontenggideon57@gmail.com>
2024-01-13 08:25:57 -0800

    Updated loading page

---
+++ app/dashboard/loading.tsx
@@ -0,0 +1,7 @@
+export default function Loading() {
+  return (
+    <div className="grid place-content-center h-screen">
+      <div className="loader grid place-content-center"></div>
+    </div>
+  );
+}
