04005aac9abd2423281f4e0bc65a04cb7bc9056a
GideonAdonteng <adontenggideon57@gmail.com>
2024-01-17 00:59:54 -0800

    Passed employee data as props to new manager form

--- app/dashboard/managers/new/page.tsx
+++ app/dashboard/managers/new/page.tsx
@@ -1,5 +1,11 @@
 import NewManagerForm from "@/components/NewManagerForm";
+import { getAllEmployees } from "@/lib/actions";
+import { notFound } from "next/navigation";
 
-export default function NewManager() {
-  return <NewManagerForm />;
+export default async function NewManager() {
+  const employeesData: Promise<Employee[]> = getAllEmployees();
+  const employees: Employee[] = await employeesData;
+
+  if (!employees) return notFound();
+  return <NewManagerForm employees={employees} />;
 }
