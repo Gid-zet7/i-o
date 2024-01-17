1c905ccb9c6c1d03da5428d5ee5995396549e7f1
GideonAdonteng <adontenggideon57@gmail.com>
2024-01-16 19:28:10 -0800

    Updated Navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,17 +1,27 @@
+"use client";
 import Link from "next/link";
 import ProfileMenu from "./ProfileMenu";
-import { getCurrentUser } from "@/lib/session";
 import AuthProvider from "./AuthProvider";
+import { useSession } from "next-auth/react";
+import ThemeToggleButton from "@/components/themeToggleButton/themeToggleButton";
 
-const Navbar = async () => {
-  const session = await getCurrentUser();
+export type NavbarProps = {
+  ColorModeContext: React.Context<{ toggleColorMode: () => void }>;
+};
+
+const Navbar = (props: NavbarProps) => {
+  const { ColorModeContext } = props;
+  // const session = await getCurrentUser();
+  const { data: session } = useSession();
+  // console.log(session);
   return (
     <nav className="py-5 px-8 border-b gap-4 sticky top-0 z-10 backdrop-blur-sm">
       <div className="mx-auto flex max-w-4xl items-center justify-between">
         <h1 className="text-3xl">
           <Link href="/">I/O</Link>
         </h1>
-        <div className="flexCenter gap-4">
+        <div className="flex gap-4">
+          <ThemeToggleButton ColorModeContext={ColorModeContext} />
           {session?.user ? (
             <>
               <ProfileMenu session={session} />
