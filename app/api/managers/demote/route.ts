7d93c1c67c41db9aefa90796044466a890ec679d
GideonAdonteng <adontenggideon57@gmail.com>
2024-01-17 09:16:33 -0800

    Updated managers api

--- app/api/managers/demote/route.ts
+++ app/api/managers/demote/route.ts
@@ -3,44 +3,48 @@ import Manager from "@/models/managerModel";
 import { verifyJwt } from "@/lib/jwt";
 
 export const DELETE = async (request: Request) => {
-  const authHeader =
-    request.headers.get("authorization") ||
-    request.headers.get("Authorization");
+  // const authHeader =
+  //   request.headers.get("authorization") ||
+  //   request.headers.get("Authorization");
 
-  // console.log(authHeader);
+  // // console.log(authHeader);
 
-  if (!authHeader?.startsWith("Bearer ")) {
-    return new Response(
-      JSON.stringify({
-        error: "unauthorized",
-      }),
-      {
-        status: 401,
-      }
-    );
-  }
+  // if (!authHeader?.startsWith("Bearer ")) {
+  //   return new Response(
+  //     JSON.stringify({
+  //       error: "unauthorized",
+  //     }),
+  //     {
+  //       status: 401,
+  //     }
+  //   );
+  // }
 
-  const token = authHeader.split(" ")[1];
-  // console.log(token);
+  // const token = authHeader.split(" ")[1];
+  // // console.log(token);
 
-  if (!token || !verifyJwt(token)) {
-    return new Response(
-      JSON.stringify({
-        error: "unauthorized",
-      }),
-      {
-        status: 401,
-      }
-    );
-  }
+  // if (!token || !verifyJwt(token)) {
+  //   return new Response(
+  //     JSON.stringify({
+  //       error: "unauthorized",
+  //     }),
+  //     {
+  //       status: 401,
+  //     }
+  //   );
+  // }
   const { id } = await request.json();
 
   if (!id) {
     return new Response("An Id is required", { status: 400 });
   }
 
+  console.log(id);
+
   await connectDB();
   const manager = await Manager.findById(id).exec();
+
+  console.log(manager);
 
   if (!manager) {
     return new Response("Manager not found", { status: 400 });
