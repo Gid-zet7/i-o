68d3a4df7234fd406aeba1eb800dd77d5a4ebd48
GideonAdonteng <adontenggideon57@gmail.com>
2024-01-19 09:50:15 -0800

    Added a slider component to show clients

---
+++ components/Slides.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import Image from "next/image";
+
+export default function Slides() {
+  return (
+    <div className="w-full inline-flex flex-nowrap overflow-hidden [mask-image:_linear-gradient(to_right,transparent_0,_black_128px,_black_calc(100%-200px),transparent_100%)">
+      <ul className="flex items-center justify-center md:justify-start [&_li]:mx-8 [&_img]:max-w-none animate-infinite-scroll">
+        <li>
+          <Image src="/facebook.svg" width={60} height={30} alt="Facebook" />
+        </li>
+        <li>
+          <Image src="/twitter.svg" width={60} height={30} alt="Disney" />
+        </li>
+        <li>
+          <Image src="/instagram.svg" width={60} height={30} alt="Airbnb" />
+        </li>
+        <li>
+          <Image src="/apple.svg" width={60} height={30} alt="Apple" />
+        </li>
+        <li>
+          <Image src="/mercedes.svg" width={60} height={30} alt="Spark" />
+        </li>
+      </ul>
+      <ul
+        className="flex items-center justify-center md:justify-start [&_li]:mx-8 [&_img]:max-w-none animate-infinite-scroll"
+        aria-hidden="true"
+      >
+        <li>
+          <Image src="/github.svg" width={60} height={30} alt="Facebook" />
+        </li>
+        <li>
+          <Image src="/messenger.svg" width={60} height={30} alt="Disney" />
+        </li>
+        <li>
+          <Image src="/forbes.svg" width={60} height={30} alt="Airbnb" />
+        </li>
+        <li>
+          <Image src="/instagram-logo.svg" width={60} height={30} alt="Apple" />
+        </li>
+        <li>
+          <Image src="/aladdin.svg" width={60} height={30} alt="Spark" />
+        </li>
+      </ul>
+    </div>
+  );
+}
