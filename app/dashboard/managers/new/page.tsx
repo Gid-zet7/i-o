34205a44fb7bfe8b45c418184003c4204f009195
GideonAdonteng <adontenggideon57@gmail.com>
2024-01-24 16:53:31 -0800

    Fetched projects and passed as props to manager form

--- app/dashboard/managers/new/page.tsx
+++ app/dashboard/managers/new/page.tsx
@@ -1,11 +1,15 @@
 import NewManagerForm from "@/components/NewManagerForm";
-import { getAllEmployees } from "@/lib/actions";
+import { getAllEmployees, getAllProjects } from "@/lib/actions";
 import { notFound } from "next/navigation";
 
 export default async function NewManager() {
   const employeesData: Promise<Employee[]> = getAllEmployees();
   const employees: Employee[] = await employeesData;
 
+  const projectsData: Promise<Project[]> = getAllProjects();
+  const projects: Project[] = await projectsData;
+
   if (!employees) return notFound();
-  return <NewManagerForm employees={employees} />;
+  if (!projects) return notFound();
+  return <NewManagerForm employees={employees} projects={projects} />;
 }
