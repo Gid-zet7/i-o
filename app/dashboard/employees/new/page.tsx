79335e3654320f8e545077f35242ef8d0179f57d
GideonAdonteng <adontenggideon57@gmail.com>
2024-01-24 16:59:34 -0800

    Fectched users and passed as props to employee form

--- app/dashboard/employees/new/page.tsx
+++ app/dashboard/employees/new/page.tsx
@@ -1,11 +1,15 @@
 import NewEmployeeForm from "@/components/NewEmployeeForm";
-import { getAllDepartments } from "@/lib/actions";
+import { getAllDepartments, getAllUsers } from "@/lib/actions";
 import { notFound } from "next/navigation";
 
 export default async function AddEmployee() {
+  const usersData: Promise<User[]> = getAllUsers();
+  const users: User[] = await usersData;
+
   const departmentsData: Promise<Department[]> = getAllDepartments();
   const departments: Department[] = await departmentsData;
 
+  if (!users) return notFound();
   if (!departments) return notFound();
-  return <NewEmployeeForm departments={departments} />;
+  return <NewEmployeeForm departments={departments} users={users} />;
 }
