549d4bfe6c3d20d0c39dfeb499c0d5fce2d83626
GideonAdonteng <adontenggideon57@gmail.com>
2024-01-24 16:54:37 -0800

    updated manager profile page

--- app/dashboard/managers/[managerId]/page.tsx
+++ app/dashboard/managers/[managerId]/page.tsx
@@ -3,9 +3,14 @@ import { getManager } from "@/lib/actions";
 import Image from "next/image";
 // import { notFound } from "next/navigation";
 import Link from "next/link";
-import { IconButton } from "@mui/material";
-import { Edit } from "@mui/icons-material";
+import { IconButton, Paper } from "@mui/material";
+import { Description, Edit } from "@mui/icons-material";
 import Typography from "@mui/material/Typography";
+import Project from "@/models/projectModel";
+import Details from "@/components/Details";
+import ProjectsCard from "@/components/ProjectsCard";
+import { title } from "process";
+import TeamCard from "@/components/TeamCard";
 
 type Params = {
   params: {
@@ -36,12 +41,14 @@ export default async function ManagerPage({ params: { managerId } }: Params) {
     const managerData: Promise<Manager> = getManager(managerId);
     const manager: Manager = await managerData;
 
+    // console.log(manager.employee);
+
     if (!manager?.employee.user.username) {
       throw new Error("Manager not found");
     }
 
     return (
-      <section key={manager._id} className="flex max-w-7xl mx-auto p-3">
+      <section className="flex max-w-7xl mx-auto p-3">
         <div className="grid gap-5 sm:grid-rows-3 sm:grid-cols-3 sm:gap-8 p-2">
           <div className="flex-1">
             <h1 className="mb-10 text-2xl font-semibold">Manager Profile</h1>
@@ -51,7 +58,9 @@ export default async function ManagerPage({ params: { managerId } }: Params) {
                 alt="manager Image"
                 width={350}
                 height={200}
-                className="rounded-3xl mb-12"
+                className="rounded-3xl mb-12 w-auto h-auto"
+                key={manager._id}
+                priority
               />
             ) : (
               <Image
@@ -59,43 +68,21 @@ export default async function ManagerPage({ params: { managerId } }: Params) {
                 alt="manager Image"
                 width={350}
                 height={200}
-                className="rounded-3xl mb-12"
+                className="rounded-3xl mb-12 w-auto h-auto"
+                key={manager._id}
+                priority
               />
             )}
-
-            <div className="flex flex-col gap-2">
-              <h1 className="text-2xl font-semibold">Employee Details</h1>
-              <div className="details">
-                <p>First name</p>
-                <p className="font-semibold blue_gradient ">
-                  {manager.employee.firstname}
-                </p>
-              </div>
-              <div className="details">
-                <p>Last name</p>
-                <p className="font-semibold blue_gradient ">
-                  {manager.employee.lastname}
-                </p>
-              </div>
-              <div className="details">
-                <p>Email</p>
-                <p className="font-semibold blue_gradient ">
-                  {manager.employee.user.email}
-                </p>
-              </div>
-              <div className="details">
-                <p>Department</p>
-                <p className="font-semibold blue_gradient ">
-                  {manager.employee.department.name}
-                </p>
-              </div>
-              <div className="details">
-                <p>Position</p>
-                <p className="font-semibold blue_gradient ">
-                  {manager.employee.position}
-                </p>
-              </div>
-            </div>
+            <h1 className="text-2xl mb-3">Details</h1>
+            <Details
+              id={manager._id}
+              username={manager.employee.user.username}
+              firstname={manager.employee.firstname}
+              lastname={manager.employee.lastname}
+              email={manager.employee.user.email}
+              department={manager.employee.department.name}
+              position={manager.employee.position}
+            />
 
             <Link href={`/dashboard/managers/${manager._id}/edit`}>
               <Typography fontSize={"h6"} color={"lightslategrey"}>
@@ -107,79 +94,102 @@ export default async function ManagerPage({ params: { managerId } }: Params) {
             </Link>
           </div>
           <div className="flex-1 p-3">
-            <h1 className="text-2xl font-semibold mb-8">Skills</h1>
-
-            <p className="mb-8">{manager.employee.skills}</p>
-
             <div>
               <h1 className="text-2xl font-semibold mb-8">Team</h1>
               <div className="flex flex-col gap-3">
                 {manager.team.map((teammate) => {
                   return (
-                    <li key={teammate._id} className="list-none details ">
-                      {teammate.user.avatarUrl ? (
-                        <div key={teammate._id} className="flex gap-4">
-                          <Image
-                            src={teammate.user.avatarUrl}
-                            width={50}
-                            height={90}
-                            alt="teammate image"
-                            className="rounded-full "
-                          />
-                          <Link
-                            href={`http://localhost:3000/dashboard/employees/${teammate._id}`}
-                          >
-                            <p className="blue_gradient text-sm md:text-xl">
-                              {teammate.user.username}
-                            </p>
-                          </Link>
-                        </div>
-                      ) : (
-                        <div className="flex gap-4">
-                          <Image
-                            src="/undraw_male_avatar_g98d.svg"
-                            width={50}
-                            height={90}
-                            alt="teammate image"
-                            className="rounded-full "
-                          />
-                          <Link
-                            href={`http://localhost:3000/dasboard/employees/${teammate._id}`}
-                          >
-                            <p className="blue_gradient">
-                              {teammate.user.username}
-                            </p>
-                          </Link>
-                        </div>
-                      )}
-                    </li>
+                    <TeamCard
+                      id={teammate._id}
+                      avartar={teammate.user.avatarUrl}
+                      username={teammate.user.username}
+                    />
+                    // <li
+                    //   key={teammate.user.username}
+                    //   className="list-none details "
+                    // >
+                    //   {teammate.user.avatarUrl ? (
+                    //     <div
+                    //       // key={teammate.user.username}
+                    //       className="flex gap-4"
+                    //     >
+                    //       <Image
+                    //         src={teammate.user.avatarUrl}
+                    //         width={50}
+                    //         height={90}
+                    //         alt="teammate image"
+                    //         className="rounded-full "
+                    //       />
+                    //       <Link
+                    //         href={`http://localhost:3000/dashboard/employees/${teammate._id}`}
+                    //       >
+                    //         <p className="blue_gradient text-sm md:text-xl">
+                    //           {teammate.user.username}
+                    //         </p>
+                    //       </Link>
+                    //     </div>
+                    //   ) : (
+                    //     <div
+                    //       // key={teammate.user.username}
+                    //       className="flex gap-4"
+                    //     >
+                    //       <Image
+                    //         src="/undraw_male_avatar_g98d.svg"
+                    //         width={50}
+                    //         height={90}
+                    //         alt="teammate image"
+                    //         className="rounded-full "
+                    //       />
+                    //       <Link
+                    //         href={`http://localhost:3000/dasboard/employees/${teammate._id}`}
+                    //       >
+                    //         <p className="blue_gradient">
+                    //           {teammate.user.username}
+                    //         </p>
+                    //       </Link>
+                    //     </div>
+                    //   )}
+                    // </li>
                   );
                 })}{" "}
               </div>
             </div>
           </div>
           <div className="flex-1 p-3">
-            <h1 className="text-2xl font-semibold mb-8">Performance</h1>
+            <h1 className="text-2xl font-semibold mb-8">Projects</h1>
 
-            <p className="mb-8">
-              {manager?.employee.performance.length
-                ? manager?.employee.performance.map((perf) => {
-                    return <li> {perf.feedback}</li>;
-                  })
-                : "Performance not appraised yet"}
-            </p>
+            {manager.projects.map((project, i) => {
+              return (
+                <ProjectsCard
+                  id={i}
+                  title={project.title}
+                  description={project.description}
+                  tasks={project.tasks}
+                />
+                // <div key={project.title}>
+                //   <h1>
+                //     Project Title: {i + 1}. {project.title}{" "}
+                //   </h1>
 
-            <p className="leading-7">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Architecto ea adipisci deserunt iste eos eaque est rerum
-              voluptatem dolores sunt explicabo, molestias amet corporis nam
-              doloremque vitae a inventore aliquam? Ex labore ipsa, nesciunt
-              ipsum quasi alias fuga aliquid fugit? Laboriosam officiis quia
-              inventore ducimus a dicta saepe. Ea, similique! Voluptatibus
-              quidem obcaecati nihil saepe ipsum mollitia numquam eveniet
-              repudiandae laborum explicabo provident tenetur cupiditate dicta
-              amet laboriosam tempora
-            </p>
+                //   <h1>Project description</h1>
+                //   <p>{project.description} </p>
+                //   <div key={i}>
+                //     <h1>Tasks</h1>
+                //     {project.tasks.map((task, j) => {
+                //       return (
+                //         <>
+                //           <div key={j}>
+                //             <p>
+                //               {j + 1}. {task.description}{" "}
+                //             </p>
+                //           </div>
+                //         </>
+                //       );
+                //     })}
+                //   </div>
+                // </div>
+              );
+            })}
           </div>
         </div>
       </section>
