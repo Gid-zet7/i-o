5b030a359a64fee9344d7be34d4aa626bb75d67e
GideonAdonteng <adontenggideon57@gmail.com>
2024-01-29 21:56:39 -0800

    Updated managerCard

--- components/ManagerCard.tsx
+++ components/ManagerCard.tsx
@@ -1,9 +1,7 @@
 "use client";
 
-import Image from "next/image";
 import Link from "next/link";
-import { Paper } from "@mui/material";
-import Typography from "@mui/material/Typography";
+import { Paper, Box, Avatar } from "@mui/material";
 
 type Props = {
   id: string;
@@ -28,45 +26,68 @@ const ManagerCard = ({
   projects,
 }: Props) => {
   return (
-    <Paper className="rounded-lg" key={id}>
-      {image ? (
-        <Link
-          href={`/dashboard/managers/${id}`}
-          className="flexCenter group relative"
-        >
-          <Image
-            src={image}
-            width={130}
-            height={80}
-            className="object-cover rounded-t-lg w-auto"
-            alt="employee image"
-          />
-        </Link>
-      ) : (
-        <div className="flexCenter flex-col rounded-full shadow-md dark:bg-slate-100 dark:text-black min-w-1/4">
-          <Link href={`/managers/${id}}`} className="flexCenter group relative">
-            <Image
-              src="/undraw_male_avatar_g98d.svg"
-              width={130}
-              height={80}
-              className="object-cover rounded-t-lg w-auto"
-              alt="employee image"
-            />
+    <section
+      style={{ fontFamily: "Montserrat" }}
+      className=" bg-[#071e34] flex font-medium items-center justify-center rounded-2xl"
+    >
+      <Paper className="w-64 mx-auto rounded-2xl px-8 py-6 shadow-lg">
+        <div className="flex items-center justify-between">
+          <Link href={`/dashboard/managers/${id}`}>
+            <span className="text-black text-sm bg-emerald-400 p-2 rounded-md">
+              View profile
+            </span>
           </Link>
+
+          {/* <span className="text-emerald-400">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"
+              />
+            </svg>
+          </span> */}
         </div>
-      )}
-      <div className="p-4">
-        <Typography fontSize={"h6"} color={"lightslategrey"}>
-          {firstname} {lastname}
-        </Typography>
-        <Typography fontSize={"h6"} color={"lightslategrey"}>
-          {department}
-        </Typography>
-        <Typography fontSize={"h6"} color={"lightslategrey"}>
-          {position}
-        </Typography>
-      </div>
-    </Paper>
+        <div className="mt-6 w-fit mx-auto">
+          <Box display="flex" flexDirection="column" alignItems="center">
+            <Avatar
+              sx={{
+                height: 100,
+                width: 100,
+                // marginBottom: 2,
+              }}
+              src={image}
+            />
+          </Box>
+        </div>
+
+        <div className="mt-8 ">
+          <h2 className=" font-bold text-2xl tracking-wide">
+            {firstname} <br /> {lastname}
+          </h2>
+        </div>
+        <p className="text-emerald-400 font-semibold mt-2.5">Active</p>
+
+        {/* <div className="h-1 w-full bg-black mt-8 rounded-full">
+          <div className="h-1 rounded-full w-2/5 bg-yellow-500 "></div>
+        </div> */}
+        <div className="mt-3 text-sm">
+          <span className="text-gray-400 font-semibold">Department: </span>
+          <span>{department}</span>
+        </div>
+        <div className="mt-3 text-sm">
+          <span className="text-gray-400 font-semibold">Position: </span>
+          <span>{position}</span>
+        </div>
+      </Paper>
+    </section>
   );
 };
 
