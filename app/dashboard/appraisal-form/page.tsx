b5c29938b934bbaec4dbe688b42ab2d741726fb3
GideonAdonteng <adontenggideon57@gmail.com>
2024-02-02 20:43:06 -0800

    Updated appraisal form page

--- app/dashboard/appraisal-form/page.tsx
+++ app/dashboard/appraisal-form/page.tsx
@@ -1,26 +1,18 @@
 "use client";
 
-import type { Metadata } from "next";
 import { getAllForms } from "@/lib/actions";
-import Forms from "@/components/Form";
-
-// export const metadata: Metadata = {
-//   title: "Forms",
-// };
+import FormsCard from "@/components/FormsCard";
+import SideMenu from "@/stories/SideMenu/SideMenu";
 
 export default async function FormsPage() {
   const formsData: Promise<Forms[]> = getAllForms();
 
   const forms = await formsData;
 
-  // const content = forms.map((form) => {
-  //   return <h1>{form.form_title} </h1>;
-  // });
-
-  // return content;
-  const content = forms.map((form) => {
-    return <Forms form={form} />;
-  });
-
-  return content;
+  return (
+    <div className="max-w-7xl mx-auto p-3">
+      <SideMenu />
+      <FormsCard forms={forms} />
+    </div>
+  );
 }
