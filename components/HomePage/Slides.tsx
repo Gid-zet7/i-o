b0239f9b58204b3d627939758a4745b7ec7ff331
GideonAdonteng <adontenggideon57@gmail.com>
2024-02-09 00:04:26 -0800

    Updated some images on the slider

--- components/HomePage/Slides.tsx
+++ components/HomePage/Slides.tsx
@@ -3,13 +3,13 @@ import Image from "next/image";
 
 export default function Slides() {
   return (
-    <div className="w-full inline-flex flex-nowrap overflow-hidden [mask-image:_linear-gradient(to_right,transparent_0,_black_128px,_black_calc(100%-200px),transparent_100%)">
+    <div className="w-full inline-flex flex-nowrap overflow-hidden [mask-image:_linear-gradient(to_right,transparent_0,_black_128px,_black_calc(100%-200px),transparent_100%) mb-10">
       <ul className="flex items-center justify-center md:justify-start [&_li]:mx-8 [&_img]:max-w-none animate-infinite-scroll">
         <li>
-          <Image src="/facebook.svg" width={60} height={30} alt="Facebook" />
+          <Image src="/fb.svg" width={60} height={30} alt="Facebook" />
         </li>
         <li>
-          <Image src="/twitter.svg" width={60} height={30} alt="Disney" />
+          <Image src="/x.svg" width={60} height={30} alt="Disney" />
         </li>
         <li>
           <Image src="/instagram.svg" width={60} height={30} alt="Airbnb" />
@@ -29,14 +29,14 @@ export default function Slides() {
           <Image src="/github.svg" width={60} height={30} alt="Facebook" />
         </li>
         <li>
-          <Image src="/messenger.svg" width={60} height={30} alt="Disney" />
+          <Image src="/fb-messenger.svg" width={60} height={30} alt="Disney" />
         </li>
         <li>
           <Image src="/forbes.svg" width={60} height={30} alt="Airbnb" />
         </li>
-        <li>
+        {/* <li>
           <Image src="/instagram-logo.svg" width={60} height={30} alt="Apple" />
-        </li>
+        </li> */}
         <li>
           <Image src="/aladdin.svg" width={60} height={30} alt="Spark" />
         </li>
