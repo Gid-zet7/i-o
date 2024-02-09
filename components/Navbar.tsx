cb1bc0dff8a25a4548f5d43e4a8e5de3f1e0b13b
GideonAdonteng <adontenggideon57@gmail.com>
2024-02-09 00:02:05 -0800

    Changed max width of the contents on the navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -16,7 +16,7 @@ const Navbar = (props: NavbarProps) => {
   // console.log(session);
   return (
     <nav className="py-5 px-8 border-b gap-4 sticky top-0 z-10 backdrop-blur-sm">
-      <div className="mx-auto flex max-w-4xl items-center justify-between">
+      <div className="mx-auto flex max-w-7xl items-center justify-between">
         <h1 className="text-3xl">
           <Link href="/">I/O</Link>
         </h1>
