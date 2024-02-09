e438ba5024cfe23c72f4ab623325cc5da4cb8291
GideonAdonteng <adontenggideon57@gmail.com>
2024-02-09 00:08:19 -0800

    I just removed a console log

--- components/EmployeeAndManager/EditEmployeeForm.tsx
+++ components/EmployeeAndManager/EditEmployeeForm.tsx
@@ -72,8 +72,6 @@ export default function EditEmployeeForm({
 
   const router = useRouter();
 
-  console.log(performance);
-
   // ------------------Skills------------------
   const addOption = () => {
     let allSkills = [...skills];
