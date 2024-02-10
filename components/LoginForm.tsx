03587a783107b6d0d75f0af822ca657bc838743f
GideonAdonteng <adontenggideon57@gmail.com>
2024-02-09 23:24:42 -0800

    Updated login form

--- components/LoginForm.tsx
+++ components/LoginForm.tsx
@@ -1,16 +1,41 @@
 "use client";
 
 import Link from "next/link";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { signIn } from "next-auth/react";
 import { useRouter } from "next/navigation";
 import { Paper, Box, Grid, TextField } from "@mui/material";
+import Snackbar from "./Snackbar";
 
 export default function LoginForm() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [isSuccess, setIsSuccess] = useState("");
   const [error, setError] = useState("");
+  const [showErrorSnackbar, setShowErrorSnackbar] = useState(false);
+  const [showSuccessSnackbar, setShowSuccessSnackbar] = useState(false);
+
+  useEffect(() => {
+    if (error) {
+      setShowErrorSnackbar(true);
+      const timer = setTimeout(() => {
+        setShowErrorSnackbar(false);
+        setError("");
+      }, 3000);
+
+      // Clear the timer when the component unmounts or when error changes
+      return () => clearTimeout(timer);
+    } else if (isSuccess) {
+      setShowSuccessSnackbar(true);
+      const timer = setTimeout(() => {
+        setShowSuccessSnackbar(false);
+        setIsSuccess("");
+      }, 3000);
+
+      // Clear the timer when the component unmounts or when error changes
+      return () => clearTimeout(timer);
+    }
+  }, [error]);
 
   const router = useRouter();
 
@@ -26,8 +51,19 @@ export default function LoginForm() {
 
       console.log(res);
 
-      if (res?.error) {
-        setError("Invalid Credentials");
+      if (
+        res?.error ===
+        `Unexpected token 'W', "Wrong password" is not valid JSON`
+      ) {
+        setError("Wrong password");
+        return;
+      }
+
+      if (
+        res?.error ===
+        `Unexpected token 'F', "Failed to login" is not valid JSON`
+      ) {
+        setError("Failed to login, check username or network");
         return;
       }
 
@@ -36,6 +72,7 @@ export default function LoginForm() {
       router.push("dashboard");
     } catch (error) {
       console.log(error);
+      return error;
     }
   };
 
@@ -44,6 +81,8 @@ export default function LoginForm() {
       <Paper className="flex items-center justify-center  h-screen  z-10">
         <Box>
           <Paper className="shadow-xl  dark:border-1 min-w-1/5 p-7 md:px-20 md:py-16 rounded-2xl z-10">
+            {showErrorSnackbar && <Snackbar message={error} />}
+            {showSuccessSnackbar && <Snackbar message={isSuccess} />}
             <h1 className="text-xl font-bold my-4 grid place-content-center">
               Sign in
             </h1>
@@ -70,11 +109,6 @@ export default function LoginForm() {
               <button className="bg-gradient-to-r from-blue-600 to-cyan-600 text-black font-bold cursor-pointer px-6 py-4 rounded-lg hover:opacity-90 ">
                 Login
               </button>
-              {error && (
-                <div className="bg-red-500 text-white w-fit text-sm py-1 px-3 rounded-md mt-2">
-                  {error}
-                </div>
-              )}
 
               <Link className="text-sm mt-3 text-right" href={"/register"}>
                 Don't have an account?{" "}
