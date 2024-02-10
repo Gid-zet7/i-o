884ec0add9a9bed7632ba09eccf28cace0b66e2e
GideonAdonteng <adontenggideon57@gmail.com>
2024-02-09 23:53:45 -0800

    Added a try catch block to handle errors gracefully

--- app/api/auth/login/route.ts
+++ app/api/auth/login/route.ts
@@ -6,18 +6,25 @@ import bcrypt from "bcrypt";
 export async function POST(request: Request) {
   const { username, password } = await request.json();
 
-  await connectDB();
+  try {
+    await connectDB();
 
-  const user = await UserModel.findOne({ username });
-  const passMatch = await bcrypt.compare(password, user.password);
+    const user = await UserModel.findOne({ username });
+    const passMatch = await bcrypt.compare(password, user.password);
 
-  if (user && passMatch) {
-    const { password, ...userWithoutPass } = user;
-    const accessToken = signJwtAccessToken(userWithoutPass);
-    const result = {
-      ...userWithoutPass,
-      accessToken,
-    };
-    return new Response(JSON.stringify(result));
-  } else return new Response(JSON.stringify(null));
+    if (!user) return new Response("Invalid username", { status: 400 });
+    if (!passMatch) return new Response("Wrong password", { status: 400 });
+
+    if (user && passMatch) {
+      const { password, ...userWithoutPass } = user;
+      const accessToken = signJwtAccessToken(userWithoutPass);
+      const result = {
+        ...userWithoutPass,
+        accessToken,
+      };
+      return new Response(JSON.stringify(result));
+    } else return new Response(JSON.stringify(null));
+  } catch (error) {
+    return new Response("Failed to login", { status: 500 });
+  }
 }
