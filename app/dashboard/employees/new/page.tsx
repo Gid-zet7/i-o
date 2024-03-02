9b34b406976140d1dd0cda0959299da30689c82d
GideonAdonteng <adontenggideon57@gmail.com>
2024-03-02 14:35:13 -0800

    Updated page

--- app/dashboard/employees/new/page.tsx
+++ app/dashboard/employees/new/page.tsx
@@ -1,6 +1,5 @@
 import NewEmployeeForm from "@/components/EmployeeAndManager/NewEmployeeForm";
 import { getAllDepartments, getAllUsers } from "@/lib/actions";
-import { notFound } from "next/navigation";
 
 export default async function AddEmployee() {
   const usersData: Promise<User[]> = getAllUsers();
@@ -9,7 +8,8 @@ export default async function AddEmployee() {
   const departmentsData: Promise<Department[]> = getAllDepartments();
   const departments: Department[] = await departmentsData;
 
-  if (!users) return notFound();
-  if (!departments) return notFound();
+  const errorMessage = `something went wrong. `;
+
+  if (!users || !departments) throw new Error(errorMessage);
   return <NewEmployeeForm departments={departments} users={users} />;
 }
