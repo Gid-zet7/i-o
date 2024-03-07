c06369d5530ca33058d12b40ca019269fc8748ff
GideonAdonteng <adontenggideon57@gmail.com>
2024-03-07 09:26:05 -0800

    Updates

--- lib/session.ts
+++ lib/session.ts
@@ -10,7 +10,7 @@ export const authOptions: NextAuthOptions = {
       credentials: {},
       async authorize(credentials: any) {
         const { username, password } = credentials;
-        const res = await fetch("http://localhost:3000/api/auth/login", {
+        const res = await fetch(`${process.env.SERVER_URL}/api/auth/login`, {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
