be0d6045e86bc6fc18a8fc89fe3048026f3742a8
GideonAdonteng <adontenggideon57@gmail.com>
2024-03-08 17:47:32 -0800

    Updated middleware

--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,6 @@
 export { default } from "next-auth/middleware";
-import { NextRequest } from "next/server";
 
 // See "Matching Paths" below to learn more
 export const config = {
   matcher: "/dashboard/:path*",
-};
-
-const middleware = (reqeust: NextRequest) => {
-  console.log(reqeust.url);
 };
